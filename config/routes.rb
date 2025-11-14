8c72eccd1ae086099e25369ca6af6dddecc32e1e
Colton Sikes <colton.sikes@gmail.com>
2025-11-14 12:17:46 +0900

    added delete function for list

--- config/routes.rb
+++ config/routes.rb
@@ -11,7 +11,7 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   root to: "lists#index"
-  resources :lists, only: [:index, :show, :new, :create] do
+  resources :lists, only: [:index, :show, :new, :create, :destroy] do
     resources :bookmarks, only: [:new, :create]
   end
   resources :bookmarks, only: [:destroy]
