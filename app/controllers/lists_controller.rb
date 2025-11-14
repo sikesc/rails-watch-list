649a89fd934354c5e975fbe957effda3c8e44110
Colton Sikes <colton.sikes@gmail.com>
2025-11-14 13:51:21 +0900

    added modal instead of separte pages for forms

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,10 +1,12 @@
 class ListsController < ApplicationController
   def index
     @lists = List.all
+    @list = List.new
   end
 
   def show
     @list = List.find(params["id"])
+    @bookmark = Bookmark.new
   end
 
   def new
@@ -20,7 +22,7 @@ class ListsController < ApplicationController
   def create
     @list = List.new(list_params)
     if @list.save
-      redirect_to list_path(@list)
+      redirect_to lists_path
     else
       render :new, status: :unprocessable_entity
     end
